ca0cfc1b73059d575cb24c0224854f5eb27bb500
Anshul Gupta <anshul624.gupta@gmail.com>
2019-05-06 16:08:31 +0530

    Update model_proxy.py

--- model_proxy.py
+++ model_proxy.py
@@ -1,7 +1,6 @@
 import keras
 import keras.backend as K
 from keras.models import Model, load_model, Sequential
-#from keras.applications.vgg16 import VGG16
 from keras.layers import Layer, Dense, Input, Dropout, merge, Multiply, Add, Masking, LSTM, BatchNormalization, Activation, TimeDistributed
 import tensorflow as tf
 import numpy as np
@@ -20,16 +19,9 @@ class JointNet():
         self.model.summary()
 
 
-    # def __call__(self, model_option):
-    #     if model_option == 'train':
-    #         return self.model
-    #     else:
-    #         return None
-
     def identity_loss(self, y_true, y_pred):
 
         return K.mean(y_pred)
-
 
 
     def bpr_triplet_loss(self, X):
@@ -50,22 +42,6 @@ class JointNet():
 
         return loss
 
-
-
-    def audio_submodel(self):
-
-        input_size = 80
-        hidden_size = 128
-
-        model = Sequential(name='sequential_1')        
-        model.add(Dense(512, activation='relu', input_shape=(2048, ), name='dense_aud1'))
-        model.add(Dropout(0.2))
-        model.add(Dense(128, activation='relu', name='dense_aud2'))
-        model.add(Dropout(0.2))
-        model.add(Dense(64, activation='relu', name='dense_aud3'))
-
-        return model
-
     
     def image_submodel(self):
 
@@ -83,27 +59,22 @@ class JointNet():
 
         grounding = Input((1, ), name='grounding')
         grounding_bar = Input((1, ), name='grounding_bar')
-        anchor_aud = Input((2048, ), name='anchor_aud')
         anchor_img = Input((2048, ), name='anchor_img')
         class_mask = Input((576, ), name='class_mask')
         class_mask_bar = Input((576, ), name='class_mask_bar')
         
 
-        anchor_aud_latent = self.audio_submodel(anchor_aud)
         anchor_img_latent = self.image_submodel(anchor_img)
       
 
-        anchor = Add()([Multiply()([grounding_bar, anchor_img_latent]), Multiply()([grounding, anchor_aud_latent])])     
-
-
         loss = merge(
-            [anchor, class_mask, class_mask_bar],
+            [anchor_img_latent, class_mask, class_mask_bar],
             mode=self.bpr_triplet_loss,
             name='loss',
             output_shape=(1, ))
 
         model = Model(
-            input=[grounding, grounding_bar, anchor_aud, anchor_img, class_mask, class_mask_bar],
+            input=[grounding, grounding_bar, anchor_img, class_mask, class_mask_bar],
             output=loss)
         model.compile(loss=self.identity_loss, optimizer=keras.optimizers.Adam(lr=0.01))
 
